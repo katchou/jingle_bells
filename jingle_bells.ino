1088aff3f32fa11262b6a366516dad85316750e6
katchou <jean.francois.jung@gmail.com>
2017-12-13 18:18:18 +0100

    beep beep

---
+++ jingle_bells.ino
@@ -0,0 +1,76 @@
+int togglePin = 4;
+int state = HIGH;
+int reading;
+int previous = LOW;
+long time = 0;
+long debounce = 100;
+
+int speakerPin = 5;
+int length = 26;
+char notes[] = "eeeeeeegcde fffffeeeeddedg";
+int beats[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
+
+int tempo = 300;
+
+void checkState() {
+  reading = digitalRead(togglePin);
+
+  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
+    if (state == HIGH)
+      state = LOW;
+    else
+      state = HIGH;
+
+    time = millis();    
+  }
+
+  previous = reading;
+}
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+void jingle() {
+    for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); // rest
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+    }
+    
+    // pause between notes
+    delay(tempo / 2); 
+  }
+}
+void mute() {
+  digitalWrite(speakerPin, LOW);
+}
+
+void setup() {
+  pinMode(togglePin, INPUT);
+  pinMode(speakerPin, OUTPUT);
+}
+void loop() {
+  checkState();
+  if (state == HIGH) {
+    jingle();
+  } else {
+    mute();
+  }
+}
